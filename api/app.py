d5afe960670009b639db951393e1ad1341be646c
Ange <s4nixd@gmail.com>
2018-11-23 14:16:48 +0100

    Still getting list of WebSite where to fetch datas

--- api/app.py
+++ api/app.py
@@ -2,10 +2,41 @@
 #
 # Author: [Sanix-darker](https://github.com/sanix-darker)
 #
-# Ressources / WebSites used to fetch data: 
+# Ressources / WebSites used to fetch data:
+# https://github.com/pyjobs/annonces
+# https://djangogigs.com/gigs/remote/
+# https://angel.co/europe/python/jobs
+# https://remoteml.com/tags/Python/
+# https://nodesk.co/remote-jobs/python/
+# https://www.codementor.io/freelance-jobs/python
+# https://www.indeed.com.ph/Remote-Python-jobs
+# https://remote4me.com/remote-python-jobs
+# https://www.irishjobs.ie/Remote-Python-Jobs
+# https://www.reed.co.uk/jobs/python-developer-remote/36144618
+# https://www.simplyhired.com/search?q=remote+python+developer&l=austin%2C+tx&job=ViO7geREaxO513t_xvNXtyVy2t9_kmX9_BjPGMevSnLgkOwH73PMww
+# https://remotees.com/remote-python-jobs
+# https://www.indeed.co.uk/Python-jobs-in-Remote
+# https://jobs.trovit.co.uk/remote-python-jobs
+# https://www.glassdoor.co.uk/Job/uk-remote-python-jobs-SRCH_IL.0,2_IN2_KO3,16.htm
+# https://www.cybercoders.com/jobs/remote-python-developer-jobs/
+# https://www.pearsonfrank.com/32655/uk-remote-python-developer
+# https://www.flexjobs.com/jobs/python
+# https://twitter.com/remotepython?lang=en
+# https://www.reddit.com/r/remotepython/
+# https://www.remotelyawesomejobs.com/remote-python-jobs/
+# https://europeremotely.com/remote-jobs/Python
+# https://www.glassdoor.com/Job/remote-python-jobs-SRCH_IL.0,6_IS11047_KO7,13.htm
+# https://www.glassdoor.com/Job/remote-python-jobs-SRCH_KO0,13.htm
+# https://www.linkedin.com/jobs/python-remote-jobs
+# https://stackoverflow.com/jobs/remote-developer-jobs-using-python
+# https://weworkremotely.com/remote-jobs/search?utf8=%E2%9C%93&term=python
+# https://www.workingnomads.co/remote-python-jobs
+# https://www.python.org/jobs/location/telecommute/
 # https://jobs.github.com/positions?description=python
 # https://pythonjobs.github.io/
 # https://www.python.org/jobs/
+# https://www.indeed.com/q-Remote-Python-Developer-jobs.html
+# https://www.indeed.com/q-Remote-Python-jobs.html
 # https://www.indeed.com/q-Python-jobs.html
 # https://www.indeed.com/q-Python-Developer-jobs.html
 # https://remoteok.io/remote-python-jobs
